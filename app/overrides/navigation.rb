2224bf1997139c2f9ad87d030250fe856e168373
Martin -nexus- Mlynář <nexus@smoula.net>
2024-01-07 00:20:38 +0100

    chore: fixed for RM5

---
+++ app/overrides/navigation.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Navigation
+  Deface::Override.new virtual_path: 'repositories/_navigation',
+                      name: 'show-repositories-hook-navigation',
+                      insert_before: 'erb[loud]:contains("label_statistics")',
+                      original: '88f120e99075ba3246901c6e970ca671d7166855',
+                      text: '<%= call_hook(:view_repositories_navigation, repository: @repository) %>'
+end
